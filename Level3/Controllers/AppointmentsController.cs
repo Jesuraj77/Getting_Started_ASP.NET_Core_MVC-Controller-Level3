181a480a676fc8014b8dd473a09813e5ea55afd3
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 15:55:55 +0530

    Code Added

---
+++ Level3/Controllers/AppointmentsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level3.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
